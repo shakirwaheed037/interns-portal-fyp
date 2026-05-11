0a0e90b6418f71d20a82f4d44941e6dc20ba5ba4
Shakir Waheed <shakir03748@gmail.com>
2026-05-11 12:18:12 +0500

    Update API base URL to use environment variable

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
     const userInfo = localStorage.getItem('userInfo');
+
     if (userInfo) {
         const { token } = JSON.parse(userInfo);
+
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
     }
+
     return config;
 });
 
